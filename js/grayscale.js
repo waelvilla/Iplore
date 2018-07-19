2a387aab56e348cf7bd8be0ce9abfa88f4eee70b
waelvilla <waelvilla@gmail.com>
2018-07-19 12:30:28 +0300

    added animations

--- js/grayscale.js
+++ js/grayscale.js
@@ -1,6 +1,8 @@
 (function($) {
   "use strict"; // Start of use strict
-
+  
+  //WOW Animation
+  new WOW().init(); 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
